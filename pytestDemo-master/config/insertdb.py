ea4d8ea0d47175b71c788126a71818a0127c8870
Pluto <498180536@qq.com>
2021-06-30 19:09:02 +0800

    数据库插入数据更新

--- pytestDemo-master/config/insertdb.py
+++ pytestDemo-master/config/insertdb.py
@@ -7,16 +7,18 @@ from faker import Factory, Faker
 import hashlib
 import uuid
 import random
+import time
+import redis
 
 string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 faker1 = Faker("zh_CN")
-fakerE = Faker()
+fakerE = Faker("en_GB")
 fakerJ = Faker("ja_JP")
-db = pymysql.connect(host='10.5.9.239',
+db = pymysql.connect(host='10.5.15.254',
                      port=3306,
-                     user='root',
-                     password='root',
-                     db='test',
+                     user='videt',
+                     password='123456',
+                     db='remote_use',
                      charset='utf8')
 # 创建游标对象cursor
 cursor = db.cursor()
@@ -24,42 +26,63 @@ cursor = db.cursor()
 cursor.execute("select version()")
 data = cursor.fetchone()
 print(" Database Version: %s" % data)
-# 删除数据
-sql = "delete from t_user where id>=0"
-cursor.execute(sql)
-db.commit()
-# 查看删除后的结果
-sql = "select * from t_user"
-cursor.execute(sql)
-data = cursor.fetchall()
-print("删除后t_user表:{}".format(data))
-# 插入数据
+# # 删除数据
+# sql = "delete from sys_user where id>=0"
+# cursor.execute(sql)
+# db.commit()
+# # 查看删除后的结果
+# sql = "select * from sys_user"
+# cursor.execute(sql)
+# data = cursor.fetchall()
+# print("删除后t_user表:{}".format(data))
+# # 插入数据
 start_num = int(input("输入开始插入数量:"))
 end_num = int(input("输入结束数量:"))
-sql = """INSERT IGNORE INTO t_user (id, username,nick_name,password,email,phone) values (%s, %s,%s,%s,%s,%s)"""
+# INSERT INTO `sys_user` VALUES (1410062870472822785, 1407878182907940865, 1407878182907940865, '2021-06-30 10:29:21', '2021-06-30 10:29:21', '0', NULL, 'testan', 'a0b923820dcc509a', '99', 'huangab ', NULL, NULL, NULL, 1, 2, 1, '管理员', NULL, NULL);
+
+sql = """INSERT IGNORE INTO sys_user (id, creator, modifier, create_time, update_time, deleted, remark, username, password, one_card_id,
+            nickname, email, phone, `type` , status, role_name, gender, id_card)
+values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
 for i in range(start_num, end_num):
-    username = "test00" + str(i)
-    nick_name = str(fakerJ.name())
+    username = "huangan" + str(i)
+    nickname = str(fakerE.name())
     ps = faker1.password()
     md5 = hashlib.md5()
     md5.update(ps.encode('utf-8'))  # 将字符串转换为MD5
     password = md5.hexdigest()
-    email = faker1.email()
+    email = faker1.email()  # 生成邮箱
     phone = faker1.phone_number()
     random_str = random.sample(string, 6)
     uuid_value = uuid.uuid1()
     uuid_str = uuid_value.hex
-    id = uuid_str
+    id2 = uuid_str  # 生成uuid
+    str1 = ''
+    id = str1.join(random.choice('0123456789') for i in range(16))
+    creator = str1.join(random.choice('0123456789') for i in range(16))
+    modifier = creator
+    create_time = time.strftime('%Y-%m-%d %H:%M:%S ', time.localtime(time.time()))  # 创建时间
+    update_time = create_time  # 更新时间
+    deleted = 0
+    remark = ''
+    one_card_id = str1.join(random.choice('0123456789') for i in range(12))
+    type = (random.randint(0, 1))  # 类型
+    status = 1
+    role = ['管理员用户', '普通用户']
+    role_name = role[random.randint(0, 1) % 2]  # 用户角色
+    gender = (random.randint(0, 1))  # 性别
+    id_card = faker1.ssn()  # 身份证
 
     try:
-        cursor.execute(sql, ( id, username, nick_name, password, email, phone))
+        cursor.execute(sql, (id, creator, modifier, create_time, update_time, deleted, remark, username, password, one_card_id,
+            nickname, email, phone, type, status, role_name, gender, id_card))
         data2 = cursor.fetchall()
-        print("------成功插入第{}条数据为{}--------".format(i, (id, username, nick_name, password, email, phone)))
+        print("------成功插入第{}条数据为{}--------".format(i, (id, username, nickname, password, email, phone)))
         db.commit()  # 提交修改
     except:
+        print("发生错误数据已回滚！")
         db.rollback()  # 发生错误回滚
 # 查看插入后的结果
-sql2 = "select * from t_user"
+sql2 = "select * from sys_user"
 cursor.execute(sql2)
 results = cursor.fetchall()  # 获取所有数据
 for row in results:
